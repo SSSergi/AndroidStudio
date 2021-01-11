c96231329e0d9f3b3f1ec47dcfb5e1bd64f7ddac
SSSergi <svila@cifpfbmoll.eu>
2021-01-11 19:43:16 +0100

    01-11-2021

--- EjercicioMenu/app/src/main/java/com/example/menuexercise/MainActivity.java
+++ EjercicioMenu/app/src/main/java/com/example/menuexercise/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.menuexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    // ListView listMenu;
     private ListView lvItems;
     private Adaptador adaptador;
 
@@ -17,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // listMenu =findViewById(R.id.ViewListID);
 
         lvItems =  (ListView) findViewById(R.id.ListViewID);
         adaptador = new Adaptador(this,getArrayItems());
         lvItems.setAdapter(adaptador);
+        lvItems.setOnItemClickListener(this);
     }
 
     private ArrayList<menuItem> getArrayItems(){
@@ -36,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
         listItems.add(new menuItem("Scroll View (TL)"));
 
         return listItems;
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+        String option = lvItems.getItemAtPosition(position).toString();
+
+        Toast.makeText(getApplicationContext(), "Clicked: ", Toast.LENGTH_SHORT).show();
     }
 }
